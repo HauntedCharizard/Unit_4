615be285a2ab93958ad8560f99ac007be8bf4104
Llanes_Antonio <llana692@hsd.k12.or.us>
2025-05-28 11:38:31 -0700

    Randomized enemy spawn

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,19 +1,34 @@
+using System.Numerics;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
 
     public GameObject enemyPrefab;
+    private float spawnRange = 9;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Instantiate(enemyPrefab, new Vector3(0, 0, 6), enemyPrefab.transform.rotation);
+
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    private UnityEngine.Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+
+        UnityEngine.Vector3 randomPos = new UnityEngine.Vector3(spawnPosX, 0, spawnPosZ);
+
+        return randomPos;
+    }
+
+
 }
